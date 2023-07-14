c63dbdbf9252864db1b1c8bc051eb6cbee805238
pavanb0 <pavanbagwe16@gmail.com>
2023-07-15 02:31:39 +0530

    init

--- main.py
+++ main.py
@@ -18,28 +18,28 @@ with open('goodmorning.txt', 'r') as f:
 
 words = [line.strip().lower() for line in lines]
 
-@app.route('/', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        files = request.files.getlist('file')
-        for file in files:
-            if file:
-                filename = file.filename
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return 'Files uploaded successfully!'
-    return render_template('upload.html')
+# @app.route('/', methods=['GET', 'POST'])
+# def upload():
+#     if request.method == 'POST':
+#         files = request.files.getlist('file')
+#         for file in files:
+#             if file:
+#                 filename = file.filename
+#                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#         return 'Files uploaded successfully!'
+#     return render_template('upload.html')
 
 
-@app.route('/gallery')
-def gallery():
-    image_files = os.listdir(app.config['UPLOAD_FOLDER'])
-    image_urls = [url_for('static', filename=f'assets/{filename}') for filename in image_files]
+# @app.route('/gallery')
+# def gallery():
+#     image_files = os.listdir(app.config['UPLOAD_FOLDER'])
+#     image_urls = [url_for('static', filename=f'assets/{filename}') for filename in image_files]
 
-    goodmorning_files = os.listdir(app.config['GOODMORNING'])
-    goodmorning_urls = [url_for('static', filename=f'assets/goodmorningimg/{filename}') for filename in goodmorning_files]
+#     goodmorning_files = os.listdir(app.config['GOODMORNING'])
+#     goodmorning_urls = [url_for('static', filename=f'assets/goodmorningimg/{filename}') for filename in goodmorning_files]
 
 
-    return render_template('gallery.html', image_urls=image_urls, goodmorning_urls=goodmorning_urls)
+#     return render_template('gallery.html', image_urls=image_urls, goodmorning_urls=goodmorning_urls)
 
 def have_common_letters(string1, string2):
     common_count = sum(1 for letter in string1 if letter in string2)
@@ -69,11 +69,11 @@ def goodmorning():
         if has_common:
             texts.append(text)
             paste_image(image, 'static/assets/goodmorningimg')       
-    return extract
+    return texts
 
-# print(goodmorning())
+print(goodmorning())
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
     
-    app.run(debug=True)
+#     app.run(debug=True)
